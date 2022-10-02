e215347b15fc838d6039d2ed314faf667ab50de0
Naoki Ikeguchi <me@s6n.jp>
2022-10-02 17:20:22 +0900

    feat: Implement other functions as well

--- examples/ffi-generic-cpp/main.cpp
+++ examples/ffi-generic-cpp/main.cpp
@@ -23,12 +23,78 @@ ByteArray byte_array(const uint8_t* src, const size_t len) {
     };
 }
 
+ByteArray ok() {
+    const uint8_t response[] = {0x90, 0x00};
+    return byte_array(response, sizeof(response));
+}
+
 ByteArray emulate(ByteArray tx) {
-    // Opens the AP in the card by selecting the DF.
-    const uint8_t expected[] = {0x00, 0xa4, 0x04, 0x0c, 0x0a, 0xd3, 0x92, 0xf0, 0x00, 0x26, 0x01, 0x00, 0x00, 0x00, 0x01};
-    if (expect(expected, tx)) {
-        const uint8_t response[] = {0x90, 0x00};
-        return byte_array(response, sizeof(response));
+    {
+        // Opens the AP in the card by selecting the DF.
+        const uint8_t expected[] = {0x00, 0xa4, 0x04, 0x0c, 0x0a, 0xd3, 0x92, 0xf0, 0x00, 0x26, 0x01, 0x00, 0x00, 0x00,
+                                    0x01};
+        if (expect(expected, tx)) {
+            return ok();
+        }
+    }
+
+    {
+        // Selects an EF that contains a certificate for user authentication.
+        const uint8_t expected[] = {0x00, 0xa4, 0x02, 0x0c, 0x02, 0x00, 0x0b};
+        if (expect(expected, tx)) {
+            return ok();
+        }
+    }
+
+    {
+        // Reads only 7 bytes from head to determine length of entire certificate.
+        const uint8_t expected[] = {0x00, 0xb0, 0x00, 0x00, 0x07};
+        if (expect(expected, tx)) {
+            const uint8_t response[] = {0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0x00};
+            return byte_array(response, sizeof(response));
+        }
+    }
+
+    {
+        // Reads entire certificate.
+        const uint8_t expected[] = {0x00, 0xb0, 0x00, 0x00, 0x0a};
+        if (expect(expected, tx)) {
+            const uint8_t response[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x90, 0x00};
+            return byte_array(response, sizeof(response));
+        }
+    }
+
+    {
+        // Selects a EF of PIN verification for signing.
+        const uint8_t expected[] = {0x00, 0xa4, 0x02, 0x0c, 0x02, 0x00, 0x1b};
+        if (expect(expected, tx)) {
+            return ok();
+        }
+    }
+
+    {
+        // Verifies a PIN for signing (AbCdEfG).
+        const uint8_t expected[] = {0x00, 0x20, 0x00, 0x80, 0x07, 0x41, 0x62, 0x43, 0x64, 0x45, 0x66, 0x47};
+        if (expect(expected, tx)) {
+            return ok();
+        }
+    }
+
+    {
+        // Selects a EF of the key-pair for signing.
+        const uint8_t expected[] = {0x00, 0xa4, 0x02, 0x0c, 0x02, 0x00, 0x1a};
+        if (expect(expected, tx)) {
+            return ok();
+        }
+    }
+
+    {
+        // Computes a signature of the digest.
+        const uint8_t expected[] = {0x80, 0x2a, 0x00, 0x80, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x00};
+        if (expect(expected, tx)) {
+            const uint8_t response[] = {0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x90, 0x00};
+            return byte_array(response, sizeof(response));
+        }
     }
 
     return tx;
@@ -48,12 +114,52 @@ ByteArray transmit(ByteArray tx) {
     return rx;
 }
 
+bool is_ok() {
+    auto err = jpki_last_error();
+    if (err == nullptr) {
+        return true;
+    }
+
+    printf("ERROR: %s\n", err);
+
+    return false;
+}
+
 int main() {
     jpki_init();
 
     auto nfc_card = jpki_new_nfc_card(transmit);
     auto card = jpki_new_card(nfc_card);
     auto jpki_ap = jpki_new_jpki_ap(card);
+    if (!is_ok()) {
+        return 1;
+    }
 
-    printf("%p\n", jpki_ap);
+    auto certificate = jpki_jpki_ap_read_certificate_auth(jpki_ap, true);
+    if (!is_ok()) {
+        return 1;
+    }
+
+    printf("CERTIFICATE: ");
+    print_byte_array(certificate);
+    putchar('\n');
+
+    free(certificate.ptr);
+
+    const char* pin = "AbCdEfG";
+    const uint8_t digest[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+    auto signature = jpki_jpki_ap_sign(jpki_ap, pin, byte_array(digest, sizeof(digest)));
+    if (!is_ok()) {
+        return 1;
+    }
+
+    printf("SIGNATURE: ");
+    print_byte_array(signature);
+    putchar('\n');
+
+    free(signature.ptr);
+
+    jpki_jpki_ap_close(jpki_ap);
+    jpki_card_close(card);
+    jpki_nfc_card_close(nfc_card);
 }
--- ffi/generic/src/lib.rs
+++ ffi/generic/src/lib.rs
@@ -1,15 +1,80 @@
 #![feature(vec_into_raw_parts)]
+#![allow(clippy::missing_safety_doc)]
 
+use jpki::ap::jpki::CertType;
 use jpki::ap::JpkiAp;
 use jpki::nfc::{Command, HandlerInCtx, Response};
 use jpki::Card;
+use std::ffi::{c_char, CStr, CString};
+use std::ptr::null_mut;
 use std::rc::Rc;
 
+static mut LAST_ERROR: Option<String> = None;
+
+fn unwrap_or<T, E>(result: Result<T, E>, default: T) -> T
+where
+    E: ToString,
+{
+    unsafe {
+        // If result is an error, sets the message to LAST_ERROR.
+        // Clears the last error otherwise.
+        LAST_ERROR = result.as_ref().err().map(|e| e.to_string());
+    }
+
+    match result {
+        Ok(value) => value,
+        Err(_) => default,
+    }
+}
+
+fn unwrap<T, E>(result: Result<T, E>) -> T
+where
+    T: Default,
+    E: ToString,
+{
+    unwrap_or(result, T::default())
+}
+
+/// A struct represents a byte array.
+/// Dependents can read it from ptr to ptr+len, and should ignore about cap.
+/// ptr can be null pointer, so dependents must check the ptr is not null.
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub struct ByteArray {
     ptr: *mut u8,
     len: usize,
     cap: usize,
+}
+
+impl Default for ByteArray {
+    fn default() -> Self {
+        Self {
+            ptr: null_mut(),
+            len: 0,
+            cap: 0,
+        }
+    }
+}
+
+impl From<Vec<u8>> for ByteArray {
+    fn from(bytes: Vec<u8>) -> Self {
+        let bytes = Box::new(bytes);
+        let (ptr, len, cap) = bytes.into_raw_parts();
+        Self { ptr, len, cap }
+    }
+}
+
+impl From<ByteArray> for Vec<u8> {
+    fn from(ByteArray { ptr, len, cap }: ByteArray) -> Self {
+        unsafe { Self::from_raw_parts(ptr, len, cap) }
+    }
+}
+
+impl ByteArray {
+    fn drain(self) {
+        let Self { ptr, len, cap } = self;
+        let _ = unsafe { Vec::from_raw_parts(ptr, len, cap) };
+    }
 }
 
 pub struct NfcCard {
@@ -18,19 +83,32 @@ pub struct NfcCard {
 
 impl HandlerInCtx for NfcCard {
     fn handle_in_ctx(&self, _: (), command: Command) -> Response {
-        let bytes = Box::new(Vec::from(command));
-        let (ptr, len, cap) = bytes.into_raw_parts();
-        let byte_array = ByteArray { ptr, len, cap };
+        let command = Vec::from(command).into();
+        let response = Response::from(Vec::from((self.delegate)(command)));
 
-        let ByteArray { ptr, len, cap } = (self.delegate)(byte_array);
-
-        unsafe { Response::from(Vec::from_raw_parts(ptr, len, cap)) }
+        command.drain();
+        response
     }
 }
 
+/// Initiates the libjpki library.
+/// Currently this occur no side effects, but it will be added in the future.
+/// So dependents should call this before using other functions.
 #[no_mangle]
 pub extern "C" fn jpki_init() {}
 
+/// Returns the latest error occurred before calling this function.
+/// If no error occurred before or failed to get the error, returns null pointer.
+#[no_mangle]
+pub extern "C" fn jpki_last_error() -> *mut c_char {
+    match unsafe { LAST_ERROR.clone() }.and_then(|e| CString::new(e).ok()) {
+        Some(str) => str.into_raw(),
+        None => null_mut(),
+    }
+}
+
+/// Creates a new NFC card delegate from the function pointer.
+/// This provided function will be called on transmitting APDU commands into the card.
 #[no_mangle]
 pub extern "C" fn jpki_new_nfc_card(
     delegate: extern "C" fn(ByteArray) -> ByteArray,
@@ -40,6 +118,14 @@ pub extern "C" fn jpki_new_nfc_card(
     Box::into_raw(Box::new(card))
 }
 
+/// Closes the NFC card.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_nfc_card_close(card: &mut NfcCard) {
+    let _ = Box::from_raw(card);
+}
+
+/// Creates a new card from the NFC card.
+/// This is an abstraction layer to support other protocols rather than NFC in the future.
 #[no_mangle]
 pub unsafe extern "C" fn jpki_new_card(nfc_card: *mut NfcCard) -> *mut Card<NfcCard, ()> {
     let nfc_card = Box::from_raw(nfc_card);
@@ -48,12 +134,109 @@ pub unsafe extern "C" fn jpki_new_card(nfc_card: *mut NfcCard) -> *mut Card<NfcC
     Box::into_raw(Box::new(card))
 }
 
+/// Closes the card.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_card_close(card: &mut Card<NfcCard, ()>) {
+    // HACK: To avoid NfcCard to be deallocated recursively, using Rc instead of Box here.
+    let _ = Rc::from_raw(card);
+}
+
+/// Opens JPKI application on the card.
 #[no_mangle]
 pub unsafe extern "C" fn jpki_new_jpki_ap(
     card: *mut Card<NfcCard, ()>,
 ) -> *mut JpkiAp<NfcCard, ()> {
     let card = Rc::from_raw(card);
-    let ap = JpkiAp::open((), card).unwrap();
 
-    Box::into_raw(Box::new(ap))
+    unwrap_or(
+        JpkiAp::open((), card).map(|ap| Box::into_raw(Box::new(ap))),
+        null_mut(),
+    )
+}
+
+/// Closes the opened JPKI application.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_jpki_ap_close(jpki_ap: *mut JpkiAp<NfcCard, ()>) {
+    let _ = Box::from_raw(jpki_ap);
+}
+
+/// Reads a certificate for signing.
+/// PIN is required.
+/// If ca is true, reads a CA certificate instead.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_jpki_ap_read_certificate_sign(
+    jpki_ap: *mut JpkiAp<NfcCard, ()>,
+    pin: *const c_char,
+    ca: bool,
+) -> ByteArray {
+    let pin = CStr::from_ptr(pin).to_bytes().to_vec();
+    let ty = match ca {
+        true => CertType::SignCA,
+        _ => CertType::Sign,
+    };
+
+    unwrap(
+        jpki_ap
+            .as_ref()
+            .unwrap()
+            .read_certificate((), ty, pin)
+            .map(|v| v.into()),
+    )
+}
+
+/// Reads a certificate for user authentication.
+/// If ca is true, reads a CA certificate instead.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_jpki_ap_read_certificate_auth(
+    jpki_ap: *mut JpkiAp<NfcCard, ()>,
+    ca: bool,
+) -> ByteArray {
+    let ty = match ca {
+        true => CertType::AuthCA,
+        _ => CertType::Auth,
+    };
+
+    unwrap(
+        jpki_ap
+            .as_ref()
+            .unwrap()
+            .read_certificate((), ty, vec![])
+            .map(|v| v.into()),
+    )
+}
+
+/// Sign the computed digest using the key-pair for user authentication.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_jpki_ap_auth(
+    jpki_ap: *mut JpkiAp<NfcCard, ()>,
+    pin: *const c_char,
+    digest: ByteArray,
+) -> ByteArray {
+    let pin = CStr::from_ptr(pin).to_bytes().to_vec();
+
+    unwrap(
+        jpki_ap
+            .as_ref()
+            .unwrap()
+            .auth((), pin, digest.into())
+            .map(|v| v.into()),
+    )
+}
+
+/// Sign the computed digest using the key-pair for signing.
+#[no_mangle]
+pub unsafe extern "C" fn jpki_jpki_ap_sign(
+    jpki_ap: *mut JpkiAp<NfcCard, ()>,
+    pin: *const c_char,
+    digest: ByteArray,
+) -> ByteArray {
+    let pin = CStr::from_ptr(pin).to_bytes().to_vec();
+
+    unwrap(
+        jpki_ap
+            .as_ref()
+            .unwrap()
+            .sign((), pin, digest.into())
+            .map(|v| v.into()),
+    )
 }
