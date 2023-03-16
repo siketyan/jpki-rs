d2e39e1a42da7bdfff4111a30247937f4469abf6
Naoki Ikeguchi <me@s6n.jp>
2023-03-16 16:44:41 +0900

    fix: Remove yasna and use der

--- cli/src/digest.rs
+++ cli/src/digest.rs
@@ -1,16 +1,31 @@
+use der::asn1::{Null, ObjectIdentifier, OctetStringRef};
+use der::{Encode, Sequence};
+
+#[derive(Sequence)]
+struct SignatureMeta {
+    oid: ObjectIdentifier,
+    null: Null,
+}
+
+#[derive(Sequence)]
+struct Signature<'a> {
+    meta: SignatureMeta,
+    bytes: OctetStringRef<'a>,
+}
+
 pub fn calculate(message: Vec<u8>) -> Vec<u8> {
     let digest = ring::digest::digest(&ring::digest::SHA1_FOR_LEGACY_USE_ONLY, &message);
-    let oid = yasna::models::ObjectIdentifier::from_slice(&[1, 3, 14, 3, 2, 26]);
+    let signature = Signature {
+        meta: SignatureMeta {
+            oid: "1.3.14.3.2.26".parse::<ObjectIdentifier>().unwrap(),
+            null: Null,
+        },
+        bytes: OctetStringRef::new(digest.as_ref()).unwrap(),
+    };
 
-    yasna::construct_der(|w| {
-        w.write_sequence(|w| {
-            w.next().write_sequence(|w| {
-                w.next().write_oid(&oid);
-                w.next().write_null();
-            });
-            w.next().write_bytes(digest.as_ref());
-        });
-    })
+    let mut vec = Vec::new();
+    signature.encode_to_vec(&mut vec).unwrap();
+    vec
 }
 
 pub fn verify(certificate: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
@@ -22,4 +37,28 @@ pub fn verify(certificate: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> boo
     );
 
     public_key.verify(&message, &signature).is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate() {
+        /*
+           SEQUENCE
+             SEQUENCE
+               ObjectIdentifier sha1 (1 3 14 3 2 26)
+               NULL
+             OCTETSTRING f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0
+        */
+        assert_eq!(
+            [
+                48, 33, 48, 9, 6, 5, 43, 14, 3, 2, 26, 5, 0, 4, 20, 247, 255, 158, 139, 123, 178,
+                224, 155, 112, 147, 90, 93, 120, 94, 12, 197, 217, 208, 171, 240,
+            ]
+            .to_vec(),
+            calculate(b"Hello".to_vec()),
+        );
+    }
 }
